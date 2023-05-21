4df91d59eb17879d6dd5e2c1b4048f699e785b15
Iamwhat <88662481+anishbro@users.noreply.github.com>
2023-05-21 21:11:44 +0545

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,40 @@
+//you can write window object if you want open() method is directly a method of window object even if you don't link it
+function anishlinkedin(){
+  open("https://www.linkedin.com/in/anish-chaudhary-91169b272/");
+}
+function anishgithub(){
+  window.open("https://github.com/anishbro");
+}
+function utsavlinkedin(){
+  window.open("");
+}
+function utsavgithub(){
+  window.open("https://github.com/siiiiiuuuu");  
+}
+function ayushlinkedin(){
+  window.open("https://www.linkedin.com/in/ayush-shah-1ba499277/?fbclid=IwAR3MZKQukARO5copCUaY9wmPepWu8igvGTrpRXx4mETzYEIOSG8yG5DuVTk");
+}
+function ayushgithubin(){
+  window.open("");
+}
+function sujallinkedin(){
+  window.open("https://www.linkedin.com/in/s-u-j-a-l-r-a-i-1a7600277/");
+}
+function sujalgithub(){
+  window.open("https://github.com/SUZAL-RAI");
+}
+function educationglobal(){
+  window.open("https://iic.edu.np/");
+}
+function utsavPortfolio(){
+  window.open("utsav.html");
+}
+function ayushPortfolio(){
+  window.open("ayush.html");
+}
+function sujalPortfolio(){
+  window.open("sujal.html");
+}
+function anishPortfolio(){
+  window.open("anish.html");
+}
